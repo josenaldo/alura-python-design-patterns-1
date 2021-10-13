2487e9837127d087b37d76cdc1a3b0a90477dcee
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-10-13 15:42:55 -0300

    Curso: Design Patterns Python I: Boas práticas de programação Aula 01 Resolução do exercício do tópico 08

---
+++ calculador_de_impostos.py
@@ -0,0 +1,17 @@
+from orcamento import Orcamento
+from impostos import ISS, ICMS
+
+class CalculadorDeImpostos(object):
+
+    def realiza_calculo(self, orcamento, imposto):
+        imposto_calculado = imposto.calcula(orcamento)
+        print(imposto_calculado)
+
+if __name__ == '__main__':
+    calculador_de_impostos = CalculadorDeImpostos()
+
+    orcamento = Orcamento(500)
+    
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+    calculador_de_impostos.realiza_calculo(orcamento, ICMS())
+
