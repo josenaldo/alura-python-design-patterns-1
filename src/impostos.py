eaa5cb0a609e69fc2989c724245cd30a6029ee33
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-10-13 22:24:26 -0300

    Curso: Design Patterns Python I: Boas práticas de programação Aula 03 Refatoração do imposto condicional. Agora o imposto condicional tem aliquota minima e máxima. Se o novo imposto condicional precisar de um método diferente de cálcular alíquota minima e máxima,ele pode sobreescrever os métodos.

--- src/impostos.py
+++ src/impostos.py
@@ -1,27 +1,93 @@
-# noinspection PyMethodMayBeStatic
-class ISS:
-    def calcula(self, orcamento):
-        return orcamento.valor * 0.1
+from abc import ABCMeta, abstractmethod
 
 
-# noinspection PyMethodMayBeStatic
-class ICMS:
+class Imposto(metaclass=ABCMeta):
+    def __init__(self, nome):
+        self.__nome = nome
+    
+    @property
+    def nome(self):
+        return self.__nome
+        
+    @abstractmethod
     def calcula(self, orcamento):
-        return orcamento.valor * 0.06
+        pass
 
-class ICPP:
+
+class ImpostoSimples(Imposto):
+
+    def __init__(self, nome, aliquota):
+        super(ImpostoSimples, self).__init__(nome)
+        self.__aliquota = aliquota
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def aliquota(self):
+        return self.__aliquota
+
     def calcula(self, orcamento):
-        if orcamento.valor > 500:
-            return orcamento.valor * 0.07
+        return orcamento.valor * self.aliquota
+
+
+class ImpostoCondicional(Imposto, metaclass=ABCMeta):
+
+    def __init__(self, nome, aliquota_minima, aliquota_maxima):
+        super(ImpostoCondicional, self).__init__(nome)
+        self.__aliquota_minima = aliquota_minima
+        self.__aliquota_maxima = aliquota_maxima
+
+    @property
+    def aliquota_minima(self):
+        return self.__aliquota_minima
+
+    @property
+    def aliquota_maxima(self):
+        return self.__aliquota_maxima
+
+    def calcula(self, orcamento):
+        if self.deve_usar_a_taxacao_maxima(orcamento):
+            return self.taxacao_maxima(orcamento)
         else:
-            return orcamento.valor * 0.05
+            return self.taxacao_minima(orcamento)
 
-class IKCV:
-    def calcula(self, orcamento):
-        if orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento):
-            return orcamento.valor * 0.10
-        else:
-            return orcamento.valor * 0.06
+    @abstractmethod
+    def deve_usar_a_taxacao_maxima(self, orcamento):
+        pass
+
+    def taxacao_minima(self, orcamento):
+        return orcamento.valor * self.aliquota_minima
+
+    def taxacao_maxima(self, orcamento):
+        return orcamento.valor * self.aliquota_maxima
+
+
+class ISS(ImpostoSimples):
+    def __init__(self):
+        super(ISS, self).__init__("ISS", 0.1)
+
+
+class ICMS(ImpostoSimples):
+    def __init__(self):
+        super(ICMS, self).__init__("ICMS", 0.06)
+
+
+class ICPP(ImpostoCondicional):
+    def __init__(self):
+        super(ICPP, self).__init__("ICPP", 0.05, 0.07)
+
+    def deve_usar_a_taxacao_maxima(self, orcamento):
+        return orcamento.valor > 500
+
+
+class IKCV(ImpostoCondicional):
+    def __init__(self):
+        super(IKCV, self).__init__("IKCV", 0.06, 0.1)
+
+    def deve_usar_a_taxacao_maxima(self, orcamento):
+        return orcamento.valor > 500 and self.__tem_item_maior_que_100_reais(orcamento)
 
     def __tem_item_maior_que_100_reais(self, orcamento):
         """
@@ -43,8 +109,3 @@ class IKCV:
 
         maiores_que_100 = [item for item in orcamento.obter_itens() if item.valor > 100]
         return len(maiores_que_100) > 0
-
-
-
-
-
